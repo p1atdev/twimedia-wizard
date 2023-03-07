cb5348884cf450c2144347549daf6184f51ff884
Plat <git@p1at.dev>
2023-03-07 19:05:41 +0900

    fix: support new version twitter api

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/whisper@0.1.2/mod.ts"
+export * from "https://deno.land/x/whisper@0.1.3/mod.ts"
 
 export * from "https://deno.land/std@0.170.0/testing/asserts.ts"
 
